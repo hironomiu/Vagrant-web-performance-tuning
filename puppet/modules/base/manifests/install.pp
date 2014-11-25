c5d746ad4f9e53b4ab253be57fd4066874fac953
hironomiu <hironomiu@example.com>
2014-11-25 21:05:01 +0900

    siegeをepelに変更

--- puppet/modules/base/manifests/install.pp
+++ puppet/modules/base/manifests/install.pp
@@ -18,9 +18,24 @@ class base::install{
         'dstat',
         'sysstat',
         'perf',
-        'siege',
         ]:
         ensure => installed,
         require => Yumrepo['base-remi'],
+    }
+
+    yumrepo { 'epel_siege':
+        descr => 'epel repo',
+        mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch',
+        enabled    => 1,
+        gpgcheck   => 1,
+        gpgkey     => 'https://fedoraproject.org/static/0608B895.txt',
+    }
+
+    package{
+        [
+        'siege',
+        ]:
+        ensure => installed,
+        require => Yumrepo['epel_siege'],
     }
 }
